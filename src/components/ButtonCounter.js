7c7ad3bfd82215daef01a59d64883e963bf7e1d6
markdenardo <mark.denardo@gmail.com>
2020-03-03 11:46:07 -0500

    Done.

---
+++ src/components/ButtonCounter.js
@@ -0,0 +1,30 @@
+// Code ClickityClick Component Here
+import React from 'react';
+
+class ButtonCounter extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+    count: 0
+    };
+  }
+
+  handleClick = () => {
+    let newCount = this.state.count + 1
+    this.setState({
+      count: newCount
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
+  }
+}
+
+export default ButtonCounter;
